e8e69115914f69aed8bd70e1a773836cb711d98f
Okan Karakaya <okankarakaya@windowslive.com>
2021-03-17 16:26:15 +0100

    Wilders!

---
+++ index.js
@@ -0,0 +1,9 @@
+const chalk = require("chalk");
+console.log(chalk.blue("Hello world!"));
+
+console.log(
+  chalk.blue("Gordon, ") +
+    chalk.red("Gege, ") +
+    chalk.yellow("Jessica, ") +
+    chalk.green("Agnes")
+);
